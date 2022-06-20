e3b78f36ed8fd2a74df080e35ef45cc386577435
onkar kotule <onkar.kotule@bitcs.in>
2022-06-20 18:02:33 +0530

    added url rating

--- src/component/common/Rating.js
+++ src/component/common/Rating.js
@@ -60,7 +60,7 @@ const Rating = ({ BikeRating, Rate, id, active, bikeID, userID }) => {
     }
     const check2 = () => {
         const valid = true
-        if (Rate === 0 && loc === "/" && userID === user?._id) {
+        if (Rate === 0 && path !== "allrerversations" ) {
             return valid;
         } else {
             return
